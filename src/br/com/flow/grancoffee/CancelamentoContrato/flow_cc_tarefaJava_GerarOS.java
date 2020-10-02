5f7980fcb091046aaa14c5f4f0cdbbf7f5f2ae39
nascimento92 <nascimento92@users.noreply.github.com>
2020-10-01 23:39:33 -0300

    news

--- src/br/com/flow/grancoffee/CancelamentoContrato/flow_cc_tarefaJava_GerarOS.java
+++ src/br/com/flow/grancoffee/CancelamentoContrato/flow_cc_tarefaJava_GerarOS.java
@@ -13,12 +13,15 @@ import br.com.sankhya.jape.dao.JdbcWrapper;
 import br.com.sankhya.jape.sql.NativeSql;
 import br.com.sankhya.jape.util.FinderWrapper;
 import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.vo.EntityVO;
 import br.com.sankhya.jape.wrapper.JapeFactory;
 import br.com.sankhya.jape.wrapper.JapeWrapper;
+import br.com.sankhya.modelcore.util.DynamicEntityNames;
 import br.com.sankhya.modelcore.util.EntityFacadeFactory;
 
 public class flow_cc_tarefaJava_GerarOS implements TarefaJava {
-	int contador = 0;
+	//int contador = 0;
+	String tipoRetirada="";
 	
 	@Override
 	public void executar(ContextoTarefa arg0) throws Exception {
@@ -56,16 +59,26 @@ public class flow_cc_tarefaJava_GerarOS implements TarefaJava {
 		}
 	}
 	
-	private void criarOsParaAhPlanta(Object idflow,BigDecimal planta,ContextoTarefa arg0) {
+	private void criarOsParaAhPlanta(Object idflow,BigDecimal planta,ContextoTarefa arg0) throws Exception {
 		Object usuarioInclusao = arg0.getCampo("SYS_USUARIOINCLUSAO");
 		
 		String quantidadeMaquinas="Quantidade Máquinas: "+contagemQuantidadeDeMaquinas(idflow,planta)+"\n";
-		String Patrimonios = "Patrimônios: \n"+getPatrimonios(idflow,planta)+"\n";
-		
+		String Patrimonios = "Patrimônios: \n"+getPatrimonios(idflow,planta)+"\n";	
 		String outrosCampos = getOutrosCampos(idflow,planta,usuarioInclusao);
 		
-		String descricao = quantidadeMaquinas+Patrimonios+outrosCampos;
-		arg0.setCampo("SYS_OS", descricao);
+		String descricao = this.tipoRetirada+quantidadeMaquinas+Patrimonios+outrosCampos;
+		
+		BigDecimal numos = gerarCabecalhoOS(idflow,descricao);
+		if(numos.intValue()!=0) {
+			int primeiro = 59;
+			geraItemOS(numos,idflow,primeiro, 1);
+			
+			int segundo = 159;
+			geraItemOS(numos,idflow,segundo, 2);
+			
+			salvaOsGerada(idflow,numos);
+		}
+		//arg0.setCampo("SYS_OS", descricao);
 	}
 	
 	private String getPatrimonios(Object idflow,BigDecimal planta) {
@@ -108,6 +121,13 @@ public class flow_cc_tarefaJava_GerarOS implements TarefaJava {
 			JapeWrapper DAO = JapeFactory.dao("AD_FORMCANCELAMENTO");
 			DynamicVO VO = DAO.findOne("IDINSTPRN=?",new Object[] { idflow });
 			if(VO!=null) {
+				//String tipoRetirada="";
+				if("1".equals(VO.asString("TIPOCANCEL"))) {
+					this.tipoRetirada="CANCELAMENTO PARCIAL \n";
+				}else {
+					this.tipoRetirada="CANCELAMENTO TOTAL \n";
+				}
+				String nomeplanta = "Planta: "+ getAdPlanta(VO.asBigDecimal("NUMCONTRATO"),planta).asString("NOMEPLAN")+"\n";
 				String multa ="";
 				if("1".equals(VO.asString("COBRARMULTA"))) {
 					multa = "Multa: SIM - "+VO.asBigDecimal("MULTA")+"\n";
@@ -139,7 +159,7 @@ public class flow_cc_tarefaJava_GerarOS implements TarefaJava {
 				String email = "E-mail: "+VO.asString("CT_EMAIL")+"\n";
 				String gestor = "Gestor do Contrato: "+usuarioInclusao(usuarioInclusao)+"\n";
 				
-				descricao=multa+ultimaCobranca+dtRetirada+endereco+integracao+restricaoHorario+acessorios+contato+email+gestor;
+				descricao=nomeplanta+multa+ultimaCobranca+dtRetirada+endereco+integracao+restricaoHorario+acessorios+contato+email+gestor;
 			}
 			
 		} catch (Exception e) {
@@ -193,5 +213,144 @@ public class flow_cc_tarefaJava_GerarOS implements TarefaJava {
 		}
 		return nomeUsuario;
 	}
+	
+	private DynamicVO getFormCancelmaento(Object idflow) throws Exception {
+		JapeWrapper DAO = JapeFactory.dao("AD_FORMCANCELAMENTO");
+		DynamicVO VO = DAO.findOne("IDINSTPRN=?",new Object[] { idflow });
+		return VO;
+	}
+	
+	private BigDecimal gerarCabecalhoOS(Object idflow, String descricao) throws Exception{
+		
+		BigDecimal numos = BigDecimal.ZERO;
+		DynamicVO form = getFormCancelmaento(idflow);
+		DynamicVO patrimonio = getUmPatrimonioDeExemplo(idflow);
+		
+		try {
+			EntityFacade dwfFacade = EntityFacadeFactory.getDWFFacade();
+			DynamicVO ModeloNPVO = (DynamicVO) dwfFacade.findEntityByPrimaryKeyAsVO("OrdemServico",new BigDecimal(552465));
+			DynamicVO NotaProdVO = ModeloNPVO.buildClone();
 
+			NotaProdVO.setProperty("DHCHAMADA", TimeUtils.getNow());
+			NotaProdVO.setProperty("DTPREVISTA", TimeUtils.dataAddDay(TimeUtils.getNow(), 7));
+			NotaProdVO.setProperty("DTFECHAMENTO", null);
+			NotaProdVO.setProperty("MODELOVISIVELAPPOS",null);
+			NotaProdVO.setProperty("NOMEMODELO",null);
+			NotaProdVO.setProperty("NUMOS",null);
+			NotaProdVO.setProperty("SITUACAO","P");
+			NotaProdVO.setProperty("CODUSUSOLICITANTE", new BigDecimal(2379));		
+			NotaProdVO.setProperty("CODUSURESP", new BigDecimal(2379));	
+			NotaProdVO.setProperty("DESCRICAO", descricao);
+			NotaProdVO.setProperty("SERIE",patrimonio.asString("CODBEM"));
+			NotaProdVO.setProperty("CODBEM",patrimonio.asString("CODBEM"));
+			NotaProdVO.setProperty("NUMCONTRATO",form.asBigDecimal("NUMCONTRATO"));
+			NotaProdVO.setProperty("CODPARC",form.asBigDecimal("CODPARC"));
+			NotaProdVO.setProperty("CODCONTATO",new BigDecimal(1));
+			NotaProdVO.setProperty("AD_MANPREVENTIVA", "N");
+			NotaProdVO.setProperty("CODATEND", new BigDecimal(2379));
+			NotaProdVO.setProperty("AD_DTPREVISTAPREV", TimeUtils.getNow());
+			NotaProdVO.setProperty("CODUSUFECH", null);
+			NotaProdVO.setProperty("DHFECHAMENTOSLA", null);
+			NotaProdVO.setProperty("TEMPOSLA", new BigDecimal(7000));
+			NotaProdVO.setProperty("TEMPOGASTOSLA", null);
+			NotaProdVO.setProperty("AD_CODIGOLIBERACAO", null);
+			NotaProdVO.setProperty("AD_TELASAC", "S");
+			NotaProdVO.setProperty("CODCOS", new BigDecimal(1));
+			NotaProdVO.setProperty("CODCENCUS", getTCSCON(form.asBigDecimal("NUMCONTRATO")).asBigDecimal("CODCENCUS"));
+			
+			dwfFacade.createEntity(DynamicEntityNames.ORDEM_SERVICO,(EntityVO) NotaProdVO);
+			numos = NotaProdVO.asBigDecimal("NUMOS");
+
+			return numos;
+
+		} catch (Exception e) {
+			System.out.println("## [flow_cc_tarefaJava_GerarOS] ## - Não foio possivel gerar o cabeçalho da OS!");
+			e.getCause();
+			e.getMessage();
+			e.printStackTrace();
+		}
+		return numos;
+	}
+	
+	private void geraItemOS(BigDecimal numos,Object idflow, int usuario, int numitem) throws Exception{
+		
+		DynamicVO patrimonio = getUmPatrimonioDeExemplo(idflow);
+		
+		try {
+			
+			EntityFacade dwfFacade = EntityFacadeFactory.getDWFFacade();
+			DynamicVO ModeloNPVO = (DynamicVO) dwfFacade.findEntityByPrimaryKeyAsVO("ItemOrdemServico",new Object[]{new BigDecimal(552465),new BigDecimal(numitem)});
+			DynamicVO NotaProdVO = ModeloNPVO.buildClone();
+			
+			NotaProdVO.setProperty("NUMOS",numos);			
+			NotaProdVO.setProperty("HRINICIAL", null);
+			NotaProdVO.setProperty("HRFINAL", null);
+			NotaProdVO.setProperty("DHPREVISTA", TimeUtils.dataAddDay(TimeUtils.getNow(), 7));
+			NotaProdVO.setProperty("DHLIMITESLA", TimeUtils.dataAddDay(TimeUtils.getNow(), 7));
+			NotaProdVO.setProperty("INICEXEC", null);
+			NotaProdVO.setProperty("TERMEXEC", null);
+			NotaProdVO.setProperty("SERIE", patrimonio.asString("CODBEM"));
+			NotaProdVO.setProperty("CODPROD", getTCIBEM(patrimonio.asString("CODBEM")).asBigDecimal("CODPROD"));
+			NotaProdVO.setProperty("CODSIT", new BigDecimal(1));
+			NotaProdVO.setProperty("CODOCOROS", new BigDecimal(2));
+			NotaProdVO.setProperty("SOLUCAO", " ");
+			NotaProdVO.setProperty("CODUSU", new BigDecimal(usuario));
+			NotaProdVO.setProperty("CORSLA", null);
+			
+			dwfFacade.createEntity(DynamicEntityNames.ITEM_ORDEM_SERVICO,(EntityVO) NotaProdVO);
+
+		} catch (Exception e) {
+			System.out.println("## [flow_cc_tarefaJava_GerarOS] ## - Não foio possivel gerar os itens da OS!");
+			e.getCause();
+			e.getMessage();
+			e.printStackTrace();
+		}
+	}
+	
+	private DynamicVO getUmPatrimonioDeExemplo(Object idflow) throws Exception {
+		JapeWrapper DAO = JapeFactory.dao("AD_PATCANCELAMENTO");
+		DynamicVO VO = DAO.findOne("IDINSTPRN=? and ROWNUM=1", new Object[] { idflow });
+		return VO;
+	}
+	
+	private DynamicVO getTCIBEM(String codbem) throws Exception{
+		JapeWrapper DAO = JapeFactory.dao("Imobilizado");
+		DynamicVO VO = DAO.findOne("CODBEM=?",new Object[] { codbem });
+		return VO;	
+	}
+	
+	private DynamicVO getTCSCON(BigDecimal contrato) throws Exception {
+		DynamicVO VO = null;
+
+		try {
+			JapeWrapper DAO = JapeFactory.dao("Contrato");
+			VO = DAO.findOne("NUMCONTRATO=?", new Object[] { contrato });
+		} catch (Exception e) {
+			e.getMessage();e.printStackTrace();
+		}
+		return VO;
+	}
+	
+	private void salvaOsGerada(Object idflow, BigDecimal numos) {
+		try {
+			
+			EntityFacade dwfFacade = EntityFacadeFactory.getDWFFacade();
+			EntityVO NPVO = dwfFacade.getDefaultValueObjectInstance("AD_OSCANCELAMENTO");
+			DynamicVO VO = (DynamicVO) NPVO;
+			
+			VO.setProperty("CODREGISTRO", new BigDecimal(1));
+			VO.setProperty("IDINSTPRN", idflow);
+			VO.setProperty("IDINSTTAR", new BigDecimal(0));
+			VO.setProperty("IDTAREFA", "UserTask_1nvh3nu");
+			VO.setProperty("NUMOS", numos);
+			
+			dwfFacade.createEntity("AD_OSCANCELAMENTO", (EntityVO) VO);
+			
+		} catch (Exception e) {
+			System.out.println("## [flow_cc_tarefaJava_GerarOS] ## - Não foio possivel Salvar a OS gerada!");
+			e.getCause();
+			e.getMessage();
+			e.printStackTrace();
+		}
+	}
 }
