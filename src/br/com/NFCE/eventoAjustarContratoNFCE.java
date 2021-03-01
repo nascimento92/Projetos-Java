cd817b13fb1fbd863437a64d4d3726817a681e8d
nascimento92 <nascimento92@users.noreply.github.com>
2021-03-01 18:32:41 -0300

    home

--- src/br/com/NFCE/eventoAjustarContratoNFCE.java
+++ src/br/com/NFCE/eventoAjustarContratoNFCE.java
@@ -2,12 +2,19 @@ package br.com.NFCE;
 
 import java.math.BigDecimal;
 
+import com.sankhya.util.TimeUtils;
+
 import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
+import br.com.sankhya.jape.EntityFacade;
 import br.com.sankhya.jape.event.PersistenceEvent;
 import br.com.sankhya.jape.event.TransactionContext;
 import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.vo.EntityVO;
 import br.com.sankhya.jape.wrapper.JapeFactory;
 import br.com.sankhya.jape.wrapper.JapeWrapper;
+import br.com.sankhya.modelcore.auth.AuthenticationInfo;
+import br.com.sankhya.modelcore.util.EntityFacadeFactory;
+import br.com.sankhya.ws.ServiceContext;
 
 public class eventoAjustarContratoNFCE implements EventoProgramavelJava {
 	
@@ -15,8 +22,8 @@ public class eventoAjustarContratoNFCE implements EventoProgramavelJava {
 	 * Objeto para inserir o contrato na nota NFCE quando o patrimonio está preenchido e o contrato está vazio
 	 * 
 	 * 09/02/2021 13:09 inserir funcionalidade para ajustar o CR do contrato.
+	 * 01/03/2021 09:23 ajustar a rotina de alteração do CR.
 	 */
-	
 	BigDecimal nota;
 	
 	public void afterDelete(PersistenceEvent arg0) throws Exception {
@@ -25,7 +32,7 @@ public class eventoAjustarContratoNFCE implements EventoProgramavelJava {
 	}
 
 	public void afterInsert(PersistenceEvent arg0) throws Exception {
-		start(arg0);
+		//start(arg0);
 		
 	}
 
@@ -63,31 +70,39 @@ public class eventoAjustarContratoNFCE implements EventoProgramavelJava {
 			BigDecimal top = VO.asBigDecimal("CODTIPOPER");
 			BigDecimal contratoAtual = VO.asBigDecimal("NUMCONTRATO");
 			nota = VO.asBigDecimal("NUNOTA");
+			BigDecimal contrato = getNumeroDoContrato(patrimonio);
 			
-			if(top.equals(new BigDecimal(1108)) && patrimonio!=null && contratoAtual.equals(new BigDecimal(0))){
+			if(top.equals(new BigDecimal(1108)) && patrimonio!=null && contratoAtual.equals(new BigDecimal(0))){				
+				if(contrato.intValue()!=0){
+					VO.setProperty("NUMCONTRATO", contrato);
+				}
+			}
 			
-					BigDecimal contrato = getNumeroDoContrato(patrimonio);
-					
-					if(contrato!=null){
-						VO.setProperty("NUMCONTRATO", contrato);
-						BigDecimal cr = getCR(contrato);
-						VO.setProperty("CODCENCUS", cr);
-						//System.out.println("-------->Contrato alterado da nota!");
-					}
-
+			if(top.equals(new BigDecimal(1108))) {
+				if(contrato.intValue()!=0){
+					BigDecimal cr = getCR(contrato);
+					VO.setProperty("CODCENCUS", cr);
+				}
 			}
 			
 		} catch (Exception e) {
-			System.out.println("------> Não foi possivel descobrir o contrato da nota NFCE: "+nota+"\n"+ e.getMessage());
+			salvarException("[start] nao foi possivel alterar o CR ou o Contrato! nota: "+nota+"\n"+e.getMessage()+"\n"+e.getCause());
 		}
 		
 	}
 	
 	private BigDecimal getNumeroDoContrato(String codbem) throws Exception{		
-		JapeWrapper DAO = JapeFactory.dao("Imobilizado");
-		DynamicVO VO = DAO.findOne("CODBEM=?",new Object[] { codbem });
+		BigDecimal numcontrato = BigDecimal.ZERO;
 		
-		BigDecimal numcontrato = VO.asBigDecimal("NUMCONTRATO");
+		if(codbem==null) {
+			codbem="0";
+		}
+		
+		JapeWrapper DAO = JapeFactory.dao("PATRIMONIO");
+		DynamicVO VO = DAO.findOne("CODBEM=?",new Object[] { codbem });
+		if(VO!=null) {
+			numcontrato = VO.asBigDecimal("NUMCONTRATO");
+		}
 		
 		return numcontrato;
 	}
@@ -99,6 +114,27 @@ public class eventoAjustarContratoNFCE implements EventoProgramavelJava {
 		BigDecimal cr = VO.asBigDecimal("CODCENCUS");
 		
 		return cr;
+	}
+	
+	private void salvarException(String mensagem) {
+		try {
+
+			EntityFacade dwfFacade = EntityFacadeFactory.getDWFFacade();
+			EntityVO NPVO = dwfFacade.getDefaultValueObjectInstance("AD_EXCEPTIONS");
+			DynamicVO VO = (DynamicVO) NPVO;
+
+			VO.setProperty("OBJETO", "eventoAjustarContratoNFCE");
+			VO.setProperty("PACOTE", "br.com.NFCE");
+			VO.setProperty("DTEXCEPTION", TimeUtils.getNow());
+			VO.setProperty("CODUSU", ((AuthenticationInfo) ServiceContext.getCurrent().getAutentication()).getUserID());
+			VO.setProperty("ERRO", mensagem);
+
+			dwfFacade.createEntity("AD_EXCEPTIONS", (EntityVO) VO);
+
+		} catch (Exception e) {
+			// aqui não tem jeito rs tem que mostrar no log
+			System.out.println("## [btn_cadastrarLoja] ## - Nao foi possivel salvar a Exception! " + e.getMessage());
+		}
 	}
 
 }
