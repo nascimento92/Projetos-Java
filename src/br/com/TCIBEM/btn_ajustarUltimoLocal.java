b9ae9e4cf41843336281de3731eb057205d5a895
nascimento92 <nascimento92@users.noreply.github.com>
2021-04-22 21:41:16 -0300

    home

---
+++ src/br/com/TCIBEM/btn_ajustarUltimoLocal.java
@@ -0,0 +1,14 @@
+package br.com.TCIBEM;
+
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+
+public class btn_ajustarUltimoLocal implements AcaoRotinaJava {
+
+	@Override
+	public void doAction(ContextoAcao arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
