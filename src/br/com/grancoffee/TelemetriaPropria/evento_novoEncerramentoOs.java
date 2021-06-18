e4ca0bb8baaeffa754709b90fa372d94b82c1062
nascimento92 <59546387+nascimento92@users.noreply.github.com>
2021-06-18 11:09:13 -0300

    home

--- src/br/com/grancoffee/TelemetriaPropria/evento_novoEncerramentoOs.java
+++ src/br/com/grancoffee/TelemetriaPropria/evento_novoEncerramentoOs.java
@@ -2,15 +2,24 @@ package br.com.grancoffee.TelemetriaPropria;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 
 import javax.swing.Timer;
 
+import com.sankhya.util.TimeUtils;
+
 import Helpers.WSPentaho;
 import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
+import br.com.sankhya.jape.EntityFacade;
 import br.com.sankhya.jape.event.PersistenceEvent;
 import br.com.sankhya.jape.event.TransactionContext;
 import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.jape.vo.EntityVO;
+import br.com.sankhya.modelcore.auth.AuthenticationInfo;
+import br.com.sankhya.modelcore.util.EntityFacadeFactory;
 import br.com.sankhya.modelcore.util.MGECoreParameter;
+import br.com.sankhya.ws.ServiceContext;
 
 public class evento_novoEncerramentoOs implements EventoProgramavelJava{
 
@@ -27,8 +36,7 @@ public class evento_novoEncerramentoOs implements EventoProgramavelJava{
 
 	@Override
 	public void afterUpdate(PersistenceEvent arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+		start(arg0);		
 	}
 
 	@Override
@@ -56,22 +64,29 @@ public class evento_novoEncerramentoOs implements EventoProgramavelJava{
 	
 	private void start(PersistenceEvent arg0) {
 		DynamicVO newVO = (DynamicVO) arg0.getVo();
-			
+		
 		String newPendente = newVO.asString("PENDENTE");
+		Timestamp hora = newVO.asTimestamp("TERMEXEC");
+		BigDecimal numos = newVO.asBigDecimal("NUMOS");
 		
-		
-		if("S".equals(newPendente)) {
-			
-			Timer timer = new Timer(10000, new ActionListener() {	
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					chamaPentaho();				
-				}
-			});
-			timer.setRepeats(false);
-			timer.start();
-			
-			newVO.setProperty("PENDENTE", "N");
+		try {
+	
+			if("S".equals(newPendente) && hora!=null) {
+				
+				newVO.setProperty("PENDENTE", "N");
+				
+				Timer timer = new Timer(10000, new ActionListener() {	
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						chamaPentaho();				
+					}
+				});
+				timer.setRepeats(false);
+				timer.start();
+					
+			}
+		} catch (Exception e) {
+			salvarException("[start] nao foi possivel chamar o pentaho! num os"+numos+"\n"+e.getMessage()+"\n"+e.getCause());
 		}
 	}
 	
@@ -90,6 +105,27 @@ public class evento_novoEncerramentoOs implements EventoProgramavelJava{
 
 		} catch (Exception e) {
 			e.getMessage();
+		}
+	}
+	
+	private void salvarException(String mensagem) {
+		try {
+
+			EntityFacade dwfFacade = EntityFacadeFactory.getDWFFacade();
+			EntityVO NPVO = dwfFacade.getDefaultValueObjectInstance("AD_EXCEPTIONS");
+			DynamicVO VO = (DynamicVO) NPVO;
+
+			VO.setProperty("OBJETO", "evento_verificaEncerramentoOS");
+			VO.setProperty("PACOTE", "br.com.grancoffee.TelemetriaPropria");
+			VO.setProperty("DTEXCEPTION", TimeUtils.getNow());
+			VO.setProperty("CODUSU", ((AuthenticationInfo) ServiceContext.getCurrent().getAutentication()).getUserID());
+			VO.setProperty("ERRO", mensagem);
+
+			dwfFacade.createEntity("AD_EXCEPTIONS", (EntityVO) VO);
+
+		} catch (Exception e) {
+			// aqui não tem jeito rs tem que mostrar no log
+			System.out.println("## [btn_cadastrarLoja] ## - Nao foi possivel salvar a Exception! " + e.getMessage());
 		}
 	}
 
