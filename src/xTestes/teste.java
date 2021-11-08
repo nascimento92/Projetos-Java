2598bb081a0cf22e5d591a62123fd9a32dda2881
nascimento92 <gabriel_maker@hotmail.com>
2021-11-08 19:41:12 -0300

    home

--- src/xTestes/teste.java
+++ src/xTestes/teste.java
@@ -1,10 +1,16 @@
 package xTestes;
 
-public class teste {
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
 
-	public static void main(String[] args) {
-		System.out.println("oii");
+public class teste implements AcaoRotinaJava{
+
+	@Override
+	public void doAction(ContextoAcao arg0) throws Exception {
+		boolean confirmarSimNao = arg0.confirmarSimNao("Atenção", "Teste", 1);
+		arg0.mostraErro(""+confirmarSimNao);
+		
 	}
-	
+
 	
 }
