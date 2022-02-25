c3476e44efc890e73145161622298a16949808fd
nascimento92 <gabriel_maker@hotmail.com>
2022-02-25 10:20:20 -0300

    home

--- src/xTestes/testando.java
+++ src/xTestes/testando.java
@@ -1,10 +1,10 @@
 package xTestes;
 
+import com.sankhya.util.TimeUtils;
 
 public class testando {
 	public static void main(String[] args) {
-		String pedido = "MGL-LU-1110670508194238";
-		String a = pedido.substring(0,3);
-		System.out.println(a);
+		
+		
 	}
 }
