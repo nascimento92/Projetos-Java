67dc798847da215aaa67e125c094b24903690070
nascimento92 <gabriel_maker@hotmail.com>
2022-03-07 21:51:14 -0300

    h

---
+++ src/br/com/gsn/Projetos/evento_valida_req_proj.java
@@ -0,0 +1,53 @@
+package br.com.gsn.Projetos;
+
+import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
+import br.com.sankhya.jape.event.PersistenceEvent;
+import br.com.sankhya.jape.event.TransactionContext;
+
+public class evento_valida_req_proj implements EventoProgramavelJava{
+	
+	//TODO::Quando selecionar o status de cancelado, preencher o cronograma cancelado e preencher a hora da finalização
+
+	@Override
+	public void afterDelete(PersistenceEvent arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterInsert(PersistenceEvent arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterUpdate(PersistenceEvent arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeCommit(TransactionContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeDelete(PersistenceEvent arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeInsert(PersistenceEvent arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeUpdate(PersistenceEvent arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
