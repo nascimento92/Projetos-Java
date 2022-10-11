48aa650c0864294d566090c0f7eb7670c987fd98
nascimento92 <gabriel_maker@hotmail.com>
2022-10-11 00:18:09 -0300

    aa

--- src/xTestes/teste.java
+++ src/xTestes/teste.java
@@ -18,19 +18,13 @@ import com.sankhya.util.TimeUtils;
 public class teste {
 
 	public static void main(String[] args) throws JSONException {
-		
-		int x = 0;
-		
-		for(int i =0; i < 10; i++) {
-			x += i;
-			System.out.println(i);
-		}
-		
-		System.out.println("\n\n"+x);
+
+		String retorno = "{\"serviceName\":\"MobileLoginSP.login\",\"status\":\"1\",\"pendingPrinting\":\"false\",\"transactionId\":\"39868C2AC9106E6B0C60423F1C8E3B8A\",\"responseBody\":{\"callID\":{\"$\":\"31E59C0195EE35E24836CF33D4940D2C\"},\"jsessionid\":{\"$\":\"UFGI5u_rHocQaKXD1ctP_XUWztWzlm4DPXbCxE5T\"},\"kID\":{\"$\":\"NEMzRjc5QTU0QTM0Njc4MzIwNkY1MzIxQzU1NEQwMjc=\\n\"},\"idusu\":{\"$\":\"NjQ4\\n\"}}}";
+		JSONObject j = new JSONObject(retorno);
+		String string = j.getJSONObject("responseBody").getJSONObject("jsessionid").getString("$");
+		System.out.println(string);
 		
 		/*
-		String retorno = "{\"id\":16257,\"created_at\":\"2018-01-26T13:48:18.000Z\",\"updated_at\":\"2021-10-25T21:18:17.000Z\",\"type\":\"Product\",\"manufacturer_id\":74960,\"category_id\":116,\"name\":\"IOGURTE CAFE DA MANHA LINHACA MORANGO E BANANA ACTIVIA 170G\",\"upc_code\":\"100136\",\"barcode\":null,\"external_id\":\"100136\",\"weight\":null,\"cost_price\":null,\"vendible_balance\":-32817.0,\"tags\":[],\"additional_barcodes\":[{\"id\":210,\"value\":\"7891025107170\"},{\"id\":609,\"value\":\"7891025112174\"},{\"id\":718,\"value\":\"7891025117742\"}],\"ncm_code\":null,\"cest_code\":null,\"url\":\"http://vmpay.vertitecnologia.com.br/api/v1/products/16257\",\"inventories\":[{\"distribution_center_id\":23,\"total_quantity\":120.0,\"committed_quantity\":0.0},{\"distribution_center_id\":128,\"total_quantity\":63.0,\"committed_quantity\":0.0},{\"distribution_center_id\":132,\"total_quantity\":0.0,\"committed_quantity\":0.0}]}";
-		JSONObject j = new JSONObject(retorno);
 		JSONArray jsonArray = j.getJSONArray("additional_barcodes");
 		for(int i = 0; i < jsonArray.length(); i++) {
 			JSONObject x = jsonArray.getJSONObject(i);
@@ -39,6 +33,7 @@ public class teste {
 			System.out.println("ID: "+int1+"\nVALOR: "+string);
 		}
 		*/
+		
 	}
 	
 	private static Timestamp buildData(String hora) {
